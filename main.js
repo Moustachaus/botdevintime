32deda31e2994d4592a2d38c0bb614764a595752
Moustachaus <moustachaus@gmail.com>
2019-01-24 22:11:16 -0500

    Bot dev in time

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Bot Ready");
+});
+
+bot.login('NTM4MTEzNzc4MTM5NjYwMjkz.DyvcKg.M3ZorM6G64M9zKqUtL2zJ-xoSnA');
